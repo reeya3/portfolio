6f0924de42e2aeff77c60c9fab775ffb3dbb6469
reeya3 <manandharreeya@gmail.com>
2020-08-29 11:54:58 +0545

    created edit and update method for portfolio

--- app/controllers/portfoliios_controller.rb
+++ app/controllers/portfoliios_controller.rb
@@ -17,5 +17,20 @@ class PortfoliiosController < ApplicationController
       end
     end
   end
+
+  def edit
+    @portfoliio_item = Portfoliio.find(params[:id])
+  end
+
+  def update
+    @portfoliio_item = Portfoliio.find(params[:id])
+    respond_to do |format|
+      if @portfoliio_item.update(params.require(:portfoliio).permit(:title, :subtitile, :body))
+        format.html { redirect_to portfoliios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 end
 
