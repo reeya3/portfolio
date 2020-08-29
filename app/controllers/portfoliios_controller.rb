e93e0652bd432b54dfb2093dd684979ab54f9d23
reeya3 <manandharreeya@gmail.com>
2020-08-29 11:26:44 +0545

    created new and create method for portfolio

--- app/controllers/portfoliios_controller.rb
+++ app/controllers/portfoliios_controller.rb
@@ -2,4 +2,20 @@ class PortfoliiosController < ApplicationController
   def index
     @portfoliio_items = Portfoliio.all
   end
+
+  def new
+    @portfoliio_item = Portfoliio.new
+  end
+
+  def create
+    @portfoliio_item = Portfoliio.new(params.require(:portfoliio).permit(:title, :subtitile, :body))
+    respond_to do |format|
+      if @portfoliio_item.save
+        format.html { redirect_to portfoliios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
+
