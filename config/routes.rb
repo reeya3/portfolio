3715f4acf7509254bed05a3bb35305d4159366be
reeya3 <manandharreeya@gmail.com>
2020-08-29 15:42:47 +0545

    customizing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   resources :blogs
-  resources :portfoliios
-  get 'pages/contact'
-  get 'pages/about'
-  get 'pages/home'
+  resources :portfoliios, expect: [:show]
+  get 'portfoliio/:id', to: 'portfoliios#show', as: 'portfoliio_show'
+  #get 'pages/contact'
+  #get 'pages/about'
+  #get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+
+  root to: 'pages#home'
+
 end
